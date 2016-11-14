66443dcfd699cc77d8e686a7de35a47bc6ef128e
Garrett Potts <gpotts@radiantblue.com>
2016-11-14 08:49:40 -0500

    Uncommitted code

--- sqs/src/main/groovy/org/ossim/sqs/Message.groovy
+++ sqs/src/main/groovy/org/ossim/sqs/Message.groovy
@@ -16,6 +16,8 @@ import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.amazonaws.services.sqs.model.SendMessageBatchRequest;
 import com.amazonaws.services.sqs.model.SendMessageBatchRequestEntry
+import com.amazonaws.services.sqs.model.DeleteMessageBatchRequest;
+import com.amazonaws.services.sqs.model.DeleteMessageBatchRequestEntry
 import com.amazonaws.services.sqs.model.ChangeMessageVisibilityRequest
 import org.apache.commons.codec.digest.DigestUtils
 import groovy.util.logging.Slf4j
@@ -54,21 +56,25 @@ class Message
       sqs.sendMessageBatch(queue, entryList as List<SendMessageBatchRequestEntry>)
    }
 
-   def receive(String queue)
+   def receive(ReceiveMessageConfig config)
    {
       log.trace ("receive(String queue) entered......")
       def result = []
       def messages
       try{
-         ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queue);
-
+         ReceiveMessageRequest receiveMessageRequest = 
+                  new ReceiveMessageRequest()
+                   .withQueueUrl(config.queue)
+                   .withWaitTimeSeconds(config.waitTimeSeconds)
+                   .withMaxNumberOfMessages(config.maxNumberOfMessages)
          messages = sqs.receiveMessage(receiveMessageRequest).messages
       }
       catch(e)
       {
-         log.error("ERROR: Unable to receive message for queue: ${queue}")
-         log.error("ERROR: ${e.toString()}")
+         log.error("ERROR: Unable to receive message for queue: ${config.queue}\n${e.toString()}")
       }
+      def deleteList = []
+      Integer entryId = 1
       messages?.each{message->
          try{
             if(checkMd5(message.mD5OfBody, message.body))
@@ -79,7 +85,22 @@ class Message
             {
                log.error("ERROR: BAD MD5 Checksum For Message: ${messageBody}")
             }
-            sqs.deleteMessage(new DeleteMessageRequest(queue, message.receiptHandle));
+            deleteList << new DeleteMessageBatchRequestEntry(entryId.toString(), message.receiptHandle)
+            ++entryId
+           // sqs.deleteMessage(new DeleteMessageRequest(config.queue, message.receiptHandle));
+         }
+         catch(e)
+         {
+            log.error("ERROR: ${e.toString()}")
+         }
+         try{
+            if(deleteList)
+            {
+               sqs.deleteMessageBatch( 
+                               new DeleteMessageBatchRequest(config.queue , 
+                                     deleteList as List<DeleteMessageBatchRequestEntry>)
+                             )
+            }
          }
          catch(e)
          {
